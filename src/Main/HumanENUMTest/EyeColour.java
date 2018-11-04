82ebbda502b15d2eba203ad8a35487c7e2ed668d
n2god <n2god@o2.pl>
2018-11-04 13:51:21 +0100

    EnumTestMethod added

---
+++ src/Main/HumanENUMTest/EyeColour.java
@@ -0,0 +1,9 @@
+package Main.HumanENUMTest;
+
+public enum EyeColour {
+    BLUE,
+    GREEN,
+    BLACK,
+    GRAY,
+    BROWN
+}
---
+++ src/Main/HumanENUMTest/HairColour.java
@@ -0,0 +1,8 @@
+package Main.HumanENUMTest;
+
+public enum HairColour {
+    RED,
+    BLACK,
+    BROWN,
+    BLONDE
+}
